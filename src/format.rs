4852434a1b79d0ef6181302792b9f4d20bbe64e5
Mason Smith <masonium@gmail.com>
2020-06-07 20:08:59 -0700

    Incorporate fixes from PR #10.
    * corrected D24S8_UNORM to D24_UNORM_S8_UINT
    * Use _UINT and _INT in place of _UI and _I, following Vulkan
    * consistent use of 'color space' instead of 'colorspace'
    * note that GL_FRAMEBUFFER_SRGB is enabled by default in `grr`

--- src/format.rs
+++ src/format.rs
@@ -4,9 +4,8 @@ use crate::__gl;
 #[repr(u32)]
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 /// The `Format` enum represents a sized internal format of texture
-/// storage. The naming convention is similar to that of the internal
-/// format OpenGL constants, with some extensions for increased
-/// consistency:
+/// storage. The naming convention closely follows that of Vulkan, but
+/// it has a close correspondence with OpenGL.
 ///
 /// * The '_UNORM' suffix denotes unsigned normalized formats. They are
 /// represented as unsigned integers internally, remapped to the [0.0,
@@ -19,23 +18,26 @@ use crate::__gl;
 /// OpenGL.
 ///
 /// * The '_SFLOAT' suffix denotes floating point formats, equivalent
-/// to the 'F' suffix.
+/// to the OpenGL 'F' suffix.
 ///
-/// * The '_I' suffix denotes signed integer formats, exposed to
-/// shaders unmodified as integers.
+/// * The '_INT' suffix denotes signed integer formats, exposed to
+/// shaders unmodified as integers. This is equivalent to the OpenGL
+/// 'I' suffix.
 ///
-/// * The '_UI' suffix denotes unsigned integer formats, exposed to
-/// shaders as unsigned integers.
+/// * The '_UINT' suffix denotes unsigned integer formats, exposed to
+/// shaders as unsigned integers. This is equivalent to the OpenGL
+/// 'UI' suffix.
 ///
-/// * The '_SRGB' suffix denotes sRGB formats, which are all unsigend
+/// * The '_SRGB' suffix denotes sRGB formats, which are all unsigned
 /// normalized integers. Textures in this format are assumed to be in
-/// the sRGB colorspace. Shaders reading from this format will
-/// automatically convert the color components to a linear colorspace,
-/// so the shader will only see linear values. When outputting to a
-/// render target with an '_SRGB' format, OpenGL will convert the
-/// color components to an sRGB colorspace iff the `GL_FRAMEBUFFER_SRGB` is
-/// enabled. Alpha components, if they exist, are treated as linear
-/// throughout.
+/// the sRGB color space. Shaders reading from this format will
+/// automatically convert the color components to a linear color
+/// space, so the shader will only see linear values.  Because
+/// `GL_FRAMEBUFFER_SRGB` is enabled by default in `grr`, when
+/// outputting from a shader to a render target with an '_SRGB'
+/// format, OpenGL will convert the color components to an sRGB color
+/// space automatically.  Alpha components, if they exist, are treated
+/// as linear throughout.
 ///
 /// Each component is followed by the number of bits used to represent
 /// it.
@@ -74,40 +76,41 @@ pub enum Format {
     R32G32B32A32_SFLOAT = __gl::RGBA32F,
 
     // signed integer formats
-    R8_I = __gl::R8I,
-    R8G8_I = __gl::RG8I,
-    R8G8B8_I = __gl::RGB8I,
-    R8G8B8A8_I = __gl::RGBA8I,
+    R8_INT = __gl::R8I,
+    R8G8_INT = __gl::RG8I,
+    R8G8B8_INT = __gl::RGB8I,
+    R8G8B8A8_INT = __gl::RGBA8I,
 
-    R16_I = __gl::R16I,
-    R16G16_I = __gl::RG16I,
-    R16G16B16_I = __gl::RGB16I,
-    R16G16B16A16_I = __gl::RGBA16I,
+    R16_INT = __gl::R16I,
+    R16G16_INT = __gl::RG16I,
+    R16G16B16_INT = __gl::RGB16I,
+    R16G16B16A16_INT = __gl::RGBA16I,
 
-    R32_I = __gl::R32I,
-    R32G32_I = __gl::RG32I,
-    R32G32B32_I = __gl::RGB32I,
-    R32G32B32A32_I = __gl::RGBA32I,
+    R32_INT = __gl::R32I,
+    R32G32_INT = __gl::RG32I,
+    R32G32B32_INT = __gl::RGB32I,
+    R32G32B32A32_INT = __gl::RGBA32I,
 
     // unsigned integer formats
-    R8_UI = __gl::R8UI,
-    R8G8_UI = __gl::RG8UI,
-    R8G8B8_UI = __gl::RGB8UI,
-    R8G8B8A8_UI = __gl::RGBA8UI,
+    R8_UINT = __gl::R8UI,
+    R8G8_UINT = __gl::RG8UI,
+    R8G8B8_UINT = __gl::RGB8UI,
+    R8G8B8A8_UINT = __gl::RGBA8UI,
 
-    R16_UI = __gl::R16UI,
-    R16G16_UI = __gl::RG16UI,
-    R16G16B16_UI = __gl::RGB16UI,
-    R16G16B16A16_UI = __gl::RGBA16UI,
+    R16_UINT = __gl::R16UI,
+    R16G16_UINT = __gl::RG16UI,
+    R16G16B16_UINT = __gl::RGB16UI,
+    R16G16B16A16_UINT = __gl::RGBA16UI,
 
-    R32_UI = __gl::R32UI,
-    R32G32_UI = __gl::RG32UI,
-    R32G32B32_UI = __gl::RGB32UI,
-    R32G32B32A32_UI = __gl::RGBA32UI,
+    R32_UINT = __gl::R32UI,
+    R32G32_UINT = __gl::RG32UI,
+    R32G32B32_UINT = __gl::RGB32UI,
+    R32G32B32A32_UINT = __gl::RGBA32UI,
 
     // sRGB normalized integer formats.
     R8G8B8_SRGB = __gl::SRGB8,
-    /// sRGB8 colorspace with a linear alpha
+
+    /// sRGB8 color space with a linear alpha
     R8G8B8A8_SRGB = __gl::SRGB8_ALPHA8,
 
     // depth and stencil formats
@@ -116,10 +119,10 @@ pub enum Format {
     D32_UNORM = __gl::DEPTH_COMPONENT32,
     D32_SFLOAT = __gl::DEPTH_COMPONENT32F,
 
-    S8_UI = __gl::STENCIL_INDEX8,
+    S8_UINT = __gl::STENCIL_INDEX8,
 
-    D24S8_UNORM = __gl::DEPTH24_STENCIL8,
-    D32_SFLOAT_S8_UI = __gl::DEPTH32F_STENCIL8,
+    D24_UNORM_S8_UINT = __gl::DEPTH24_STENCIL8,
+    D32_SFLOAT_S8_UINT = __gl::DEPTH32F_STENCIL8,
 }
 
 impl Format {
@@ -132,21 +135,22 @@ impl Format {
     pub fn base_format(self) -> BaseFormat {
         use Format::*;
         match self {
-            R8_UNORM | R16_UNORM | R8_SNORM | R16_SNORM | R8_I | R16_I | R32_I | R8_UI | R16_UI
-            | R32_UI | R16_SFLOAT | R32_SFLOAT => BaseFormat::R,
+            R8_UNORM | R16_UNORM | R8_SNORM | R16_SNORM | R8_INT | R16_INT | R32_INT | R8_UINT
+            | R16_UINT | R32_UINT | R16_SFLOAT | R32_SFLOAT => BaseFormat::R,
 
-            R8G8_UNORM | R16G16_UNORM | R8G8_SNORM | R16G16_SNORM | R8G8_I | R16G16_I
-            | R32G32_I | R8G8_UI | R16G16_UI | R32G32_UI | R16G16_SFLOAT | R32G32_SFLOAT => {
-                BaseFormat::RG
-            }
+            R8G8_UNORM | R16G16_UNORM | R8G8_SNORM | R16G16_SNORM | R8G8_INT | R16G16_INT
+            | R32G32_INT | R8G8_UINT | R16G16_UINT | R32G32_UINT | R16G16_SFLOAT
+            | R32G32_SFLOAT => BaseFormat::RG,
 
-            R8G8B8_UNORM | R16G16B16_UNORM | R8G8B8_SNORM | R16G16B16_SNORM | R8G8B8_I
-            | R16G16B16_I | R32G32B32_I | R8G8B8_UI | R16G16B16_UI | R32G32B32_UI
+            R8G8B8_UNORM | R16G16B16_UNORM | R8G8B8_SNORM | R16G16B16_SNORM | R8G8B8_INT
+            | R16G16B16_INT | R32G32B32_INT | R8G8B8_UINT | R16G16B16_UINT | R32G32B32_UINT
             | R16G16B16_SFLOAT | R32G32B32_SFLOAT => BaseFormat::RGB,
 
             R8G8B8A8_UNORM | R16G16B16A16_UNORM | R8G8B8A8_SNORM | R16G16B16A16_SNORM
-            | R8G8B8A8_I | R16G16B16A16_I | R32G32B32A32_I | R8G8B8A8_UI | R16G16B16A16_UI
-            | R32G32B32A32_UI | R16G16B16A16_SFLOAT | R32G32B32A32_SFLOAT => BaseFormat::RGBA,
+            | R8G8B8A8_INT | R16G16B16A16_INT | R32G32B32A32_INT | R8G8B8A8_UINT
+            | R16G16B16A16_UINT | R32G32B32A32_UINT | R16G16B16A16_SFLOAT | R32G32B32A32_SFLOAT => {
+                BaseFormat::RGBA
+            }
 
             R8G8B8_SRGB => BaseFormat::RGB,
 
@@ -154,9 +158,9 @@ impl Format {
 
             D32_SFLOAT | D16_UNORM | D24_UNORM | D32_UNORM => BaseFormat::Depth,
 
-            S8_UI => BaseFormat::Stencil,
+            S8_UINT => BaseFormat::Stencil,
 
-            D32_SFLOAT_S8_UI | D24S8_UNORM => BaseFormat::DepthStencil,
+            D32_SFLOAT_S8_UINT | D24_UNORM_S8_UINT => BaseFormat::DepthStencil,
         }
     }
 }
